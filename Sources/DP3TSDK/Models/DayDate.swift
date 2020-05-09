07570882c8bf0a561b266ac3fd9d51fa22c2fc00
Roman Pochtaruk <developmentios1@gmail.com>
2020-05-09 18:45:15 +0300

    created method for get ephID

--- Sources/DP3TSDK/Models/DayDate.swift
+++ Sources/DP3TSDK/Models/DayDate.swift
@@ -7,34 +7,34 @@
 import Foundation
 
 /// Date model wich is always rounded to just contain the current day
-struct DayDate: Codable, CustomStringConvertible, Equatable, Hashable, Comparable {
-    let timestamp: TimeInterval
-
-    init(date: Date = Date()) {
-        var calendar = Calendar.current
-        calendar.timeZone = Default.shared.parameters.crypto.timeZone
-        let components = calendar.dateComponents([.year, .day, .month], from: date)
-        timestamp = calendar.date(from: components)!.timeIntervalSince1970
-    }
-
-    public func getNext() -> DayDate {
-        let nextDay = Date(timeIntervalSince1970: timestamp).addingTimeInterval(.day)
-        return DayDate(date: nextDay)
-    }
-
-    var dayMin: Date {
-        return Date(timeIntervalSince1970: timestamp)
-    }
-
-    var dayMax: Date {
-        return dayMin.addingTimeInterval(.day)
-    }
-
-    static func < (lhs: DayDate, rhs: DayDate) -> Bool {
-        return lhs.timestamp < rhs.timestamp
-    }
-
-    var description: String {
-        return "<DayDate \(Date(timeIntervalSince1970: timestamp))>"
-    }
+public struct DayDate: Codable, CustomStringConvertible, Equatable, Hashable, Comparable {
+  let timestamp: TimeInterval
+  
+  init(date: Date = Date()) {
+    var calendar = Calendar.current
+    calendar.timeZone = Default.shared.parameters.crypto.timeZone
+    let components = calendar.dateComponents([.year, .day, .month], from: date)
+    timestamp = calendar.date(from: components)!.timeIntervalSince1970
+  }
+  
+  public func getNext() -> DayDate {
+    let nextDay = Date(timeIntervalSince1970: timestamp).addingTimeInterval(.day)
+    return DayDate(date: nextDay)
+  }
+  
+  var dayMin: Date {
+    return Date(timeIntervalSince1970: timestamp)
+  }
+  
+  var dayMax: Date {
+    return dayMin.addingTimeInterval(.day)
+  }
+  
+  public static func < (lhs: DayDate, rhs: DayDate) -> Bool {
+    return lhs.timestamp < rhs.timestamp
+  }
+  
+  public var description: String {
+    return "<DayDate \(Date(timeIntervalSince1970: timestamp))>"
+  }
 }
